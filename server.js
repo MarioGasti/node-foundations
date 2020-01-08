bab244766ebc395f3ac38b775b983f2daf9d2c9b
Mario Gastón Ferrufino <mferrufino@ibm.com>
2020-01-08 12:18:58 -0300

    First Commit

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
 const router = require('./src/router');
 
-
 const app = express();
-const port = process.env.PORT || 3100;
-
-app.use(cors());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const port = process.env.PORT || 3000;
 
 app.use(router);
 
 app.listen(port, err => {
     if (err) throw new Error(err);
-    console.log(`Server running at ${port}.`);
+    else console.log(`Server running at ${ port }.`);
 });
--- src/router.js
+++ src/router.js
@@ -1,18 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
-const db = require('./db/cloudant');
+const example = require('./example/example');
 
-const verifySignUp = require('../jwt/verifySignUp');
-
-router.get('/news/find', db.find);
-router.get('/news/get/:_id', db.get);
-router.post('/news/post', db.post);
-router.put('/news/put/:_id/:_rev', db.put);
-router.delete('/news/delete/:_id/:_rev', db.delete);
-
-router.post('/login', db.login);
-router.post('/register', [verifySignUp.checkDuplicatedNameOrEmail], db.registerUser);
-
+router.get('/', example.helloWorld);
 
 module.exports = router;
